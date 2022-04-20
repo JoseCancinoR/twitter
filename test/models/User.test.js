cb210575006f6de03d33f7ac1efc2511c69663d7
Jose <jluiscancino115@gmail.com>
2022-04-20 10:01:36 -0500

    prueba-set

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -4,9 +4,10 @@ describe("Unit Tests for User class", () => {
 
     test('Add getters', () => {
         const user = new User(1, "JoseCancinoR", "Jose", "Bio")
-        expect(user.getUsername).toBe("JoseCancinoR")
-        expect(user.getBio).toBe("Bio")
-        expect(user.getDateCreation).not.toBeUndefined()
-        expect(user.getLastUpdate).not.toBeUndefined()
+        user.setUsername = "JoseCancinoR"
+        expect(user.username).toBe("JoseCancinoR")
+
+        user.setBio = "New Bio"
+        expect(user.bio).toBe("New Bio")
     });
 })
